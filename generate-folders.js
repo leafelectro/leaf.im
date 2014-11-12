5d957d1083965d7f79bde11a57be21b8d2d01ddd
Leaf <leafelectro@gmail.com>
2014-11-12 16:50:42 +0100

    Added redirs.js to keep FE and script DRY

--- generate-folders.js
+++ generate-folders.js
@@ -1,10 +1,23 @@
 var fs = require('fs');
+var vm = require('vm');
 
-var redirs = [{'urls': ['soundcloud', 'sc', 's'], 'dest': 'https://soundcloud.com/leaf'}];
+var redirs = [];
+
+var text = fs.readFileSync('_includes/redirs.js', 'utf8');
+
+// Fills in redirs.
+eval(text);
 
 redirs.forEach(function (redir) {
     redir.urls.forEach(function (url) {
-        fs.mkdirSync(url);
-        fs.writeFileSync(url + '/index.html', '---\nlayout: default\n---\n', 'utf8');
+        if (process.argv[2] === '-v') {
+            console.log(url);
+        } else if (process.argv[2] === '-d') {
+            fs.unlinkSync(url + '/index.html');
+            fs.rmdirSync(url);
+        } else {
+            fs.mkdirSync(url);
+            fs.writeFileSync(url + '/index.html', '---\nlayout: default\n---\n', 'utf8');
+        }
     });
 });
